c4ca5bb2b8d573aaaf0e0bb2f70dfaec102d5e83
Leonardo <leonardo.fernandesf@gmail.com>
2017-08-03 13:11:16 -0400

    colocando o pod do firebase

---
+++ SCW-MUSASHI/Pods/Target Support Files/Pods-SCW-MUSASHI/Pods-SCW-MUSASHI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SCW_MUSASHI : NSObject
+@end
+@implementation PodsDummy_Pods_SCW_MUSASHI
+@end
---
+++ SCW-MUSASHI/Pods/Target Support Files/Pods-SCW-MUSASHI/Pods-SCW-MUSASHI-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SCW_MUSASHIVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SCW_MUSASHIVersionString[];
+
